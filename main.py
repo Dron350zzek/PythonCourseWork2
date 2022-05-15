455a9b458d78055244f490522cbde3dca8b7ef2d
dron350zzek <dron350zek@gmail.com>
2022-05-15 20:21:41 +0400

    find errors

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from itertools import count
 import os
 import string
 def AddLinuxSection(title):
+    maindir = os.getcwd()
+    os.chdir(SelectedDisk + ":")
     str = ("\ntitle "
           + title + "\n"
           + "ls (hd0,0)/" + title + ".iso && partnew (hd0,3) 0x00 /"+ title + ".iso\n"
@@ -9,12 +11,10 @@ def AddLinuxSection(title):
           + "echo -e \\r\\n " + "\n"
           + "map --hook" + "\n"
           + "chainloader (0xff)" + "\n")
-    with open("menu.lst", "r+") as m:
+    with open(SelectedDisk + ':\\'+ "menu.lst", "r+") as m:
         m.seek (0, 2)
         m.write (str)
-    return str
-
-def ConfBcdNoEfi():
+    os.chdir(maindir)
     return
 
 def AddWindowsInstall(title):
@@ -22,7 +22,6 @@ def AddWindowsInstall(title):
     os.system("7zip\\7z e " + title + " -oC:\\UwU\\ " + wim + " -r")
     ListFoldersInDisk = os.listdir(SelectedDisk + ":\\WININST")
     i = len(ListFoldersInDisk)
-    #set InstallDir=WININST\win{i}
     newfolder = "WIN" + str(i)
     addtofile = "set InstallDir=WININST\\" + newfolder
     os.mkdir(SelectedDisk + ":\\WININST\\" + newfolder)
@@ -45,19 +44,30 @@ def AddWindowsInstall(title):
         os.chdir(SelectedDisk + ":\\boot")
         os.system('bcdedit /store bcd /copy {default} /d "WIN' + str(i) + '" > temp')
         with open ("temp", "r") as tt: guid = tt.read()
-        guid = guid[guid.find('{'):len(guid)-1]
-        os.system('bcdedit /store bcd /set ' + guid + ' device "ramdisk=[boot]\\WININST\\' + newfolder + '\\boot.wim,{7619dcc8-fafe-11d9-b411-000476eba25f}')
-        os.system('bcdedit /store bcd /set ' + guid + ' osdevice "ramdisk=[boot]\\WININST\\' + newfolder + '\\boot.wim,{7619dcc8-fafe-11d9-b411-000476eba25f}')
+        guid = guid[guid.find('{'):len(guid)-2]
+        os.system(f'bcdedit /store bcd /set {guid} device "ramdisk=[boot]\\WININST\\{newfolder}\\boot.wim"')
+        os.system(f'bcdedit /store bcd /set {guid} osdevice "ramdisk=[boot]\\WININST\\{newfolder}\\boot.wim"')
         os.chdir(maindir)
         return
     else:
+        wininstall = ('title WINSETUP\n' +
+                    'map --unhook\n' +
+                    'map --unmap=0:0xff && map --rehook\n' +
+                    'savedefault +1\n' +
+                    'ls /shifthd.bat > nul || find --set-root --devices=hf /shifthd.bat\n' +
+                    '/shifthd.bat\n' +
+                    'if "%RDSK%"=="" pause Fatal error while swapping hard disk order, cannot continue! && configfile /menu.lst\n' +
+                    'root (hd%RDSK%,0)\n' +
+                    'chainloader /BOOTMGR\n')
         maindir = os.getcwd()
         os.chdir(SelectedDisk + ":\\")
-        with open ("menu.lst", "r+") as menu: data = menu.read();
+        with open ("menu.lst", "r+") as menu: data = menu.read(); menu.write('\n'+ wininstall)
         os.chdir(maindir)
         return
 
 def CopyMainFiles(Disk):
+
+    os.mkdir('WININST')
     os.system ("xcopy boot " + Disk + ":\\boot /e /i /h")
     os.system ("xcopy efi " + Disk + ":\\efi /e /i /h")
     os.system ("xcopy WININST " + Disk + ":\\WININST /e /i /h")
@@ -70,6 +80,7 @@ def CopyMainFiles(Disk):
     os.system ("copy menu.lst " + Disk + ":\\")
     os.system ("copy moba.exe " + Disk + ":\\")
     os.system ("copy shifthd.bat " + Disk + ":\\")
+    os.rmdir('WININST')
 
 def GetListIsoFiles():
     files = os.listdir()
@@ -107,12 +118,12 @@ def FormatDiskAndConfig(NameDisk):# will format this drive and config
 def AskFirst():
     print("Are you first time work with this drive?(y/n)")
     a1 = input()
-    if (a1 is "y") or (a1 is "Y"):
+    if (a1 == "y") or (a1 == "Y"):
         #FORMAT DRIVE, copy mgr,grub,menuE
         print("OK i'll format you'r drive")
         FormatDiskAndConfig(SelectedDisk)
         return
-    elif (a1 is "n") or (a1 is "N"):
+    elif (a1 == "n") or (a1 == "N"):
         CopyFromDiskToHere(SelectedDisk, "menu.lst")
         return
     else: AskFirst()
@@ -120,7 +131,7 @@ def AskFirst():
 def interface():
     print("1 - Add File\nF - Format and configure disk\n0 - Exit")
     a2 = input()
-    if a2 is "1":
+    if a2 == "1":
         print("Selected command 1\n")
         isos = GetListIsoFiles()
         print("Which file do you want to add?")
@@ -132,25 +143,25 @@ def interface():
             SelectedFile = isos[NumberFile - 1]
             print("What type of file is it?\n1 - UNIX\n2 - Windows(Setup)\n3 - I don't know(default)")
             TypeFile = int(input())
-            if TypeFile is 1:
+            if TypeFile == 1:
                 CopyFileToRootDisk(SelectedDisk,SelectedFile)
                 AddLinuxSection(SelectedFile)
-                CopyFileToRootDisk(SelectedDisk,"menu.lst")
+               # CopyFileToRootDisk(SelectedDisk,"menu.lst")
                 interface()
-            elif TypeFile is 2:
+            elif TypeFile == 2:
                 print("WINDOWS\n\n\n\n")
                 AddWindowsInstall(SelectedFile)
                 interface()
-            elif TypeFile is 3:
+            elif TypeFile == 3:
                 print("uniq\n\n")
                 interface()
             else:
                 print("ERROR, unknow's type of file")
                 interface()
-    elif a2 is "0":
+    elif a2 == "0":
 
         return
-    elif a2 is "F":
+    elif a2 == "F":
         FormatDiskAndConfig(SelectedDisk)
         interface()
     else:
